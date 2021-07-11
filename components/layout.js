0c741deb66b001fe864f152bd8920d9d40ad9156
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2021-07-11 22:50:01 +0900

    Use CssBaseline in _app.js and Remove global.css

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Container from '@material-ui/core/Container'
-import CssBaseline from '@material-ui/core/CssBaseline'
 import Head from 'next/head'
 import Header from './header'
 import Footer from './footer'
@@ -10,22 +9,19 @@ export const siteTitle = 'tanaken0515.com'
 
 export default function Layout({ children }) {
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth='lg'>
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content={`${name}'s official website.`}
-          />
-          <meta name="og:title" content={siteTitle} />
-          <meta name="twitter:card" content="summary_large_image" />
-        </Head>
-        <Header title={siteTitle} />
-        <main>{children}</main>
-        <Footer title={siteTitle} name={name} />
-      </Container>
-    </React.Fragment>
+    <Container maxWidth='lg'>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content={`${name}'s official website.`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <Header title={siteTitle} />
+      <main>{children}</main>
+      <Footer title={siteTitle} name={name} />
+    </Container>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '../styles/global.css'
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
