68a918b95fcd1096f8ffab31e81654431f38ce71
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2021-07-22 12:38:41 +0900

    use Typography and Link in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Layout, { name, siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
 import { makeStyles } from '@material-ui/core/styles'
 import Avatar from '@material-ui/core/Avatar'
+import Link from '@material-ui/core/Link'
+import Typography from '@material-ui/core/Typography'
 
 const useStyles = makeStyles((theme) => ({
   avatar: {
@@ -22,17 +23,19 @@ export default function Home() {
       </Head>
       <section>
         <Avatar alt={name} src="/images/profile.png" className={classes.avatar} />
-        <h1 className={utilStyles.heading2Xl}>{name}</h1>
-      </section>
-      <section className={utilStyles.headingMd}>
-        <p>
+        <Typography variant='h4' gutterBottom>{name}</Typography>
+        <Typography variant='body1'>
           ソフトウェアエンジニアをやっています。Rubyと犬が好きです。<br/>
           <br/>
-          東京在住ですが、鹿児島のRubyコミュニティである<a href="https://k-ruby.github.io/">K-Ruby</a>のメンバーです。
+          東京在住ですが、鹿児島のRubyコミュニティである
+          <Link href='https://k-ruby.github.io/' target='_blank' rel='noopener'>K-Ruby</Link>
+          のメンバーです。
           興味のある分野はWebアプリケーションの開発とCRE(Customer Reliability Engineering)と自然言語処理で、飼っている犬はゴールデンレトリバーです。<br/>
           <br/>
-          現在はGMOペパボ株式会社で<a href="https://suzuri.jp">SUZURI</a>の開発に携わっています。
-        </p>
+          現在はGMOペパボ株式会社で
+          <Link href='https://suzuri.jp' target='_blank' rel='noopener'>SUZURI</Link>
+          の開発に携わっています。
+        </Typography>
       </section>
     </Layout>
   )
