f65a86d49d268dc391d8b35146bf7ee25278123d
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2021-07-25 08:20:11 +0900

    Use Tab

--- components/Profile.js
+++ components/Profile.js
@@ -1,5 +1,8 @@
-import Avatar from '@material-ui/core/Avatar';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Avatar from '@material-ui/core/Avatar'
+import Typography from '@material-ui/core/Typography'
+import Link from '@material-ui/core/Link'
 
 const useStyles = makeStyles((theme) => ({
   avatar: {
@@ -13,6 +16,20 @@ export default function Profile() {
   const classes = useStyles()
 
   return (
-    <Avatar src="/images/profile.png" className={classes.avatar}/>
+    <>
+      <Avatar src="/images/profile.png" className={classes.avatar}/>
+      <Typography variant='body1'>
+        ソフトウェアエンジニアをやっています。Rubyと犬が好きです。<br/>
+        <br/>
+        東京在住ですが、鹿児島のRubyコミュニティである
+        <Link href='https://k-ruby.github.io/' target='_blank' rel='noopener'>K-Ruby</Link>
+        のメンバーです。
+        興味のある分野はWebアプリケーションの開発とCRE(Customer Reliability Engineering)と自然言語処理で、飼っている犬はゴールデンレトリバーです。<br/>
+        <br/>
+        現在はGMOペパボ株式会社で
+        <Link href='https://suzuri.jp' target='_blank' rel='noopener'>SUZURI</Link>
+        の開発に携わっています。
+      </Typography>
+    </>
   )
 }
