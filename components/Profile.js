c8368e2c76ba65daee461c0269638e8ddcb797cb
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2021-07-25 10:42:29 +0900

    Update Profile.js

--- components/Profile.js
+++ components/Profile.js
@@ -1,35 +1,56 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Avatar from '@material-ui/core/Avatar'
+import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
-import Link from '@material-ui/core/Link'
+import IconButton from '@material-ui/core/IconButton'
+import { GitHub, Pets, Twitter } from '@material-ui/icons'
 
 const useStyles = makeStyles((theme) => ({
-  avatar: {
-    width: theme.spacing(16),
-    height: theme.spacing(16),
-    margin: theme.spacing(2),
+  root: {
+    paddingTop: theme.spacing(2),
   },
+  avatar: {
+    width: '100%',
+    height: 'auto',
+  },
+  profileBody: {
+    paddingTop: theme.spacing(2),
+  }
 }))
 
 export default function Profile() {
+  const name = 'tanaken0515'
   const classes = useStyles()
 
   return (
     <>
-      <Avatar src="/images/profile.png" className={classes.avatar}/>
-      <Typography variant='body1'>
-        ソフトウェアエンジニアをやっています。Rubyと犬が好きです。<br/>
-        <br/>
-        東京在住ですが、鹿児島のRubyコミュニティである
-        <Link href='https://k-ruby.github.io/' target='_blank' rel='noopener'>K-Ruby</Link>
-        のメンバーです。
-        興味のある分野はWebアプリケーションの開発とCRE(Customer Reliability Engineering)と自然言語処理で、飼っている犬はゴールデンレトリバーです。<br/>
-        <br/>
-        現在はGMOペパボ株式会社で
-        <Link href='https://suzuri.jp' target='_blank' rel='noopener'>SUZURI</Link>
-        の開発に携わっています。
-      </Typography>
+      <Grid container spacing={2} className={classes.root}>
+        <Grid item xs={4}>
+          <Avatar alt={name} src="/images/profile.png" variant='square' className={classes.avatar}/>
+        </Grid>
+        <Grid item xs={8}>
+          <Typography variant='h6' component='h2'>
+            {name}
+          </Typography>
+          <Typography variant='caption'>
+            田中 健太郎 Kentaro TANAKA
+          </Typography>
+          <Typography variant='body2' className={classes.profileBody}>
+            ソフトウェアエンジニアをやっています。<br/>
+            Rubyと犬が好きです。
+          </Typography>
+          <IconButton href={`https://github.com/${name}`} target='_blank' rel='noopener'>
+            <GitHub />
+          </IconButton>
+          <IconButton href={`https://twitter.com/${name}`} target='_blank' rel='noopener'>
+            <Twitter />
+          </IconButton>
+          <IconButton href='https://twitter.com/latte20200623' target='_blank' rel='noopener'>
+            <Pets />
+          </IconButton>
+        </Grid>
+      </Grid>
     </>
   )
 }
